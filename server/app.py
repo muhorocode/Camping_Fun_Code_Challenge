8e216ffe610175dd1463170c788c381736f4e55d
Elijah Kamanga <elijahkamanga1@gmail.com>
2025-11-03 21:30:40 +0300

    Add database configuration

--- server/app.py
+++ server/app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
 
 app=Flask(__name__)
+
+#database configuration
+app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL', 'sqlite:///camping.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db=SQLAlchemy(app)
+migrate=Migrate(app,db)
 
 @app.route('/')
 def hello():
@@ -8,4 +18,3 @@ def hello():
 
 if __name__=='__main__':
     app.run(debug=True,port=5555)
-    
